59be8ad776e8a989ce8e1393716e4cd28b707664
Marco Pesenti Gritti <mpg@redhat.com>
2007-02-24 11:38:02 +0100

    Add snapshot script

---
+++ build-snapshot.sh
@@ -0,0 +1,12 @@
+VERSION=0.33
+DATE=`date +%Y%m%d`
+RELEASE=2.1
+TARBALL=olpc-artwork-$VERSION-$RELEASE.${DATE}git.tar.gz
+
+rm olpc-artwork-$VERSION.tar.gz
+
+make distcheck
+
+mv olpc-artwork-$VERSION.tar.gz $TARBALL
+scp $TARBALL mpg@devserv.devel.redhat.com:~
+rm $TARBALL
