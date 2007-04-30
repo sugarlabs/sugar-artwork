070f8d05874f3bf73e71c30d19b9db7b550e73c9
Marco Pesenti Gritti <marco@localhost.localdomain>
2007-04-30 16:31:56 +0200

    More work on the toolbox notebook theme

--- gtk/engine/sugar-style.c
+++ gtk/engine/sugar-style.c
@@ -17,6 +17,8 @@
  * Boston, MA 02111-1307, USA.
  */
 
+#include <string.h>
+
 #include "sugar-style.h"
 #include "sugar-rc-style.h"
 
@@ -24,6 +26,8 @@ static void sugar_style_init       (SugarStyle      *style);
 static void sugar_style_class_init (SugarStyleClass *klass);
 
 GType sugar_type_style = 0;
+
+static GtkStyleClass *parent_class;
 
 void
 sugar_style_register_type (GTypeModule *module)
@@ -53,7 +57,44 @@ sugar_style_init (SugarStyle *style)
 }
 
 static void
+sugar_style_draw_extension(GtkStyle        *style,
+                           GdkWindow       *window,
+                           GtkStateType     state_type,
+                           GtkShadowType    shadow_type,
+                           GdkRectangle    *area,
+                           GtkWidget       *widget,
+                           const char      *detail,
+                           int              x,
+                           int              y,
+                           int              width,
+                           int              height,
+                           GtkPositionType  gap_side)
+{
+    cairo_t *cr;
+
+    cr = gdk_cairo_create (window);
+
+    if (strcmp(detail, "tab") == 0) {
+        gdk_cairo_set_source_color(cr, &style->bg[state_type]);
+        cairo_rectangle(cr, x, y, width, height);
+        cairo_fill(cr);
+    } else {
+        parent_class->draw_extension(style, window, state_type,
+		                             shadow_type, area, widget, detail,
+		                             x, y, width, height, gap_side);
+    }
+    
+    cairo_destroy(cr);
+}
+
+static void
 sugar_style_class_init (SugarStyleClass *klass)
 {
-    GtkStyleClass *style_class = GTK_STYLE_CLASS (klass);
+    GtkStyleClass *style_class = GTK_STYLE_CLASS(klass);
+    
+    parent_class = g_type_class_peek_parent(klass);
+    
+    style_class->draw_extension = sugar_style_draw_extension;
 }
+
+
