9c8f5151f48ecca4c75f2be9f5722f47ec0844a5
Benjamin Berg <benjamin@sipsolutions.net>
2007-05-21 19:52:50 +0200

    Ported over the testing code from gtk-engines.
    I'll try to get converage reports and see if something is missing from the
    tests. I also need to check where all those leaks that valgrind reports in
    GTK+ are comming from and if it is save to ignore them.

--- test/gtk-engine-test.py
+++ test/gtk-engine-test.py
@@ -47,8 +47,8 @@ class Base:
 
 settings = gtk.settings_get_default()
 	
-if settings.get_property('gtk-theme-name') != 'olpc':
-	settings.set_string_property('gtk-theme-name', 'olpc', '')
+if settings.get_property('gtk-theme-name') != 'sugar':
+	settings.set_string_property('gtk-theme-name', 'sugar', '')
 
 base = Base()
 base.main()
