sugarlabs/sugar-artwork

commit b2b0f26119d395140b3dd8011ff8272f17d21279

Style insensitive text - SL #4139
This is the same as in GTK2 gtkrc rule: fg[INSENSITIVE] = $button_grey

Signed-off-by: Manuel Quiñones <manuq@laptop.org>

gtk3/theme/gtk-widgets.css.em
@@ -119,6 +119,10 @@ icon_large = icon_base * 5
     -GtkWidget-text-handle-height: 55px;
 }
 
+*:insensitive {
+    color: @button_grey;
+}
+
 /* Backgrounds and windows */
 
 GtkWindow {
@@ -220,7 +224,6 @@ GtkToggleButton.button:active {
     border-radius: 0px;
     padding: 0px;
     background-color: @white;
-    color: @black;
 }
 
 GtkTreeView {

commit 599c6730decba9c36dab4949dcb4d350898bc12d

Make text in a textview packed inside a Palette black, SL #4160
This can be seen in the DescriptionButton in activity toolbar
in GTK+3 activities. The regression was introduced in
b2b0f26119d395140b3dd8011ff8272f17d21279, but making the text
in a textview packed in a Palette black explicitly is the
better approach anyhow.

Signed-off-by: Simon Schampijer <simon@laptop.org>
Acked-by: Manuel Quiñones <manuq@laptop.org>

gtk3/theme/gtk-widgets.css.em
@@ -407,6 +407,10 @@ SugarPaletteWindowWidget {
     color: @white;
 }
 
+SugarPaletteWindowWidget .view {
+    color: @black;
+}
+
 SugarPaletteMenuWidget {
     background-color: @black;
 }